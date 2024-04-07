7a7011fd6e8fd934217c010405eff30a43af12c7
Wiktor Krawczak <wiko3034gmail.com>
2024-04-07 13:12:10 +0200

    zad3

--- geometry/ColoredCircle.java
+++ geometry/ColoredCircle.java
@@ -12,16 +12,5 @@ public class ColoredCircle extends Circle {
         return color;
     }
 
-    public static void main(String[] args) {
-        Point center = new Point(3, 4);
-        double radius = 5.0;
-        String color = "red";
-        ColoredCircle coloredCircle = new ColoredCircle(center, radius, color);
-        
-        System.out.println("Środek koła: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
-        System.out.println("Promień koła: " + coloredCircle.getRadius());
-        System.out.println("Obwód koła: " + coloredCircle.calculatePerimeter());
-        System.out.println("Pole powierzchni koła: " + coloredCircle.getArea());
-        System.out.println("Kolor koła: " + coloredCircle.getColor());
+
     }
-}
